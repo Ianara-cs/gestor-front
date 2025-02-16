ba7ed87bf1409e7912d0904c411e21034a75272a
Ianara-cs <ianarasilva333@gmail.com>
2025-02-16 17:00:03 -0300

    feat: inserindo a navegação no menu

--- src/shared/components/menu/menu.tsx
+++ src/shared/components/menu/menu.tsx
@@ -1,12 +1,62 @@
+import { HomeOutlined, LaptopOutlined } from '@ant-design/icons'
 import { ContainerLogoName, ContainerMenu, LogoMenu, NameCompany } from './menu.styled'
+import type { MenuProps } from 'antd'
+import { Menu as MenuAnt } from 'antd'
+import { useNavigate } from 'react-router'
+import { MenuRoutesEnum } from '../../../modules/menus/routes'
+import { useState } from 'react'
+
+type MenuItem = Required<MenuProps>['items'][number]
 
 const Menu = () => {
+  const navigate = useNavigate()
+  const [current, setCurrent] = useState('1')
+
+  const items: MenuItem[] = [
+    {
+      key: 'home',
+      label: 'Principal',
+      icon: <HomeOutlined />,
+    },
+    {
+      key: 'menus',
+      label: 'Menus',
+      icon: <LaptopOutlined />,
+      children: [
+        {
+          key: 'menus_view',
+          label: 'Visualizar',
+          onClick: () => navigate(MenuRoutesEnum.MENU),
+        },
+        {
+          key: 'menus_insert',
+          label: 'Inserir',
+          onClick: () => navigate(MenuRoutesEnum.MENUS_INSERT),
+        },
+      ],
+    },
+  ]
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    setCurrent(e.key)
+  }
+
   return (
     <ContainerMenu>
       <ContainerLogoName>
         <LogoMenu />
         <NameCompany>Gestor CG</NameCompany>
       </ContainerLogoName>
+      <MenuAnt
+        defaultSelectedKeys={['1']}
+        defaultOpenKeys={['sub1']}
+        style={{ width: 240 }}
+        selectedKeys={[current]}
+        mode="inline"
+        theme="dark"
+        onClick={onClick}
+        items={items}
+      />
     </ContainerMenu>
   )
 }
