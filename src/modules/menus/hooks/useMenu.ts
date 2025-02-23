5d3756038a80df37f976757ef91fb7df7a02083f
Ianara-cs <ianarasilva333@gmail.com>
2025-02-22 22:18:24 -0300

    feat: modal de confirmacao para excluir menu

--- src/modules/menus/hooks/useMenu.ts
+++ src/modules/menus/hooks/useMenu.ts
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router'
 import { useMenuReducer } from '../../../store/reducers/menuReducer/useMenuReducer'
 
 export const useMenu = () => {
+  const [menuIdDelete, setMenuIdDelete] = useState<string | undefined>()
   const { menus, setMenus } = useMenuReducer()
   const { request } = useRequests()
   const [menusFiltered, setMenusFiltered] = useState<MenuType[]>([])
@@ -33,20 +34,38 @@ export const useMenu = () => {
     }
   }
 
-  const handleDeleteMenu = async (menuId: string) => {
-    await request(URL_MENU_ID.replace('{menuId}', `${menuId}`), MethodsEnum.DELETE)
-    await request(URL_MENU, MethodsEnum.GET, setMenus)
-  }
-
   const handleEditMenu = async (menuId: string) => {
     navigate(MenuRoutesEnum.MENU_EDIT.replace(':menuId', `${menuId}`))
   }
 
+  const handleDeleteMenu = async () => {
+    await request(
+      URL_MENU_ID.replace('{menuId}', `${menuIdDelete}`), 
+      MethodsEnum.DELETE,
+      undefined, undefined,
+      'Cardápio deletado!'
+    )
+    await request(URL_MENU, MethodsEnum.GET, setMenus)
+    setMenuIdDelete(undefined)
+  }
+
+  const handleCloseModalDelete = () => {
+    setMenuIdDelete(undefined)
+  }
+
+  const handleOpenModalDelete = (menuId: string) => {
+    setMenuIdDelete(menuId)
+  }
+
+
   return {
     menusFiltered,
+    openModalDelete: !!menuIdDelete,
     handleOnClick,
     onSearch,
     handleDeleteMenu,
     handleEditMenu,
+    handleOpenModalDelete,
+    handleCloseModalDelete,
   }
 }
--- src/modules/menus/screens/MenuScreen.tsx
+++ src/modules/menus/screens/MenuScreen.tsx
@@ -3,22 +3,25 @@ import { ColumnsType } from 'antd/es/table'
 import Table from '../../../shared/components/table/Table'
 import Screen from '../../../shared/components/screen/Screen'
 import Button from '../../../shared/components/buttons/button/button'
-import { Input } from 'antd'
+import { Input, Modal } from 'antd'
 import { FlexJustifyBetween, FlexJustifyCenter } from '../../../shared/components/styles/display.styled'
 import { LimitedContainer } from '../../../shared/components/styles/limited.styled'
 import { useMenu } from '../hooks/useMenu'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons'
 const { Search } = Input
 
 
 const MenuScreen = () => {
   const { 
+    menusFiltered,
+    openModalDelete, 
     handleOnClick, 
-    menusFiltered, 
     onSearch, 
     handleDeleteMenu, 
     handleEditMenu,
+    handleCloseModalDelete,
+    handleOpenModalDelete,
   } = useMenu()
 
   const columns: ColumnsType<MenuType> = useMemo(() => [
@@ -43,7 +46,7 @@ const MenuScreen = () => {
       render: (_, menu) => 
       <FlexJustifyCenter>
         <Button margin="0px 16px 0px 0px" onClick={() => handleEditMenu(menu.id)} icon={<EditOutlined />}>Editar</Button>
-        <Button danger onClick={() => handleDeleteMenu(menu.id)} icon={<DeleteOutlined />}>Deletar</Button>
+        <Button danger onClick={() => handleOpenModalDelete(menu.id)} icon={<DeleteOutlined />}>Deletar</Button>
       </FlexJustifyCenter>
     },
   ], [])
@@ -59,6 +62,16 @@ const MenuScreen = () => {
         },
       ]}
     >
+      <Modal
+          title="Atenção!"
+          open={openModalDelete}
+          onOk={handleDeleteMenu}
+          onCancel={handleCloseModalDelete}
+          okText="Sim"
+          cancelText="Cancelar"
+        >
+          <p>Tem certeza que deseja excluir o menu?</p>
+        </Modal>
       <FlexJustifyBetween margin="16px 0px">
         <LimitedContainer width={240}>
           <Search placeholder="Nome do item" onSearch={onSearch} enterButton />
